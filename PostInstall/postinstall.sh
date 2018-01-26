639f269e0990882e0f44f45fc9b80a80d4386707
njmccorkle <35677754+njmccorkle@users.noreply.github.com>
2018-01-25 22:05:29 -0600

    Update postinstall.sh

--- PostInstall/postinstall.sh
+++ PostInstall/postinstall.sh
@@ -11,7 +11,6 @@ verify_root() {
 	fi
 }
 
-
 verify_args () {
         echo "verifying args "
         echo "$#"
@@ -60,7 +59,29 @@ set_hostname () {
         ifdown ens160 && ifup ens160
  }
  
- 
+ install_filebeat() {
+	# Create Beats source list
+	echo "deb https://packages.elastic.co/beats/apt stable main" |  sudo tee -a /etc/apt/sources.list.d/beats.list
+
+	#Get Elasticsearch GPG key
+	wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+
+	#Update repos and install filebeat
+	apt-get update
+	apt-get install filebeat
+
+	#download and move filebeat configuration
+	wget -qO https://raw.githubusercontent.com/njmccorkle/Ubuntu/master/PostInstall/filebeat.yml
+	cp filebeat.yml /etc/filebeat/filebeat.yml
+
+	#move private key
+	wget -qO https://raw.githubusercontent.com/njmccorkle/Ubuntu/master/PostInstall/filebeat/logstash-forwarder.crt
+	mkdir -p /etc/pki/tls/certs
+	cp logstash-forwarder.crt /etc/pki/tls/certs/
+
+	sudo systemctl restart filebeat
+	sudo systemctl enable filebeat
+ }
  
 verify_root
 verify_args "$@"
@@ -74,3 +95,4 @@ fi
 set_hostname "$@"
 install_system_packages
 install_snmpd
+install_filebeat
