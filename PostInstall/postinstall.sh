cedcd99f70ccf313f5d1f759955e420fa9dcbf1a
njmccorkle <35677754+njmccorkle@users.noreply.github.com>
2018-04-13 22:06:32 -0500

    Update repository definition

--- PostInstall/postinstall.sh
+++ PostInstall/postinstall.sh
@@ -63,7 +63,8 @@ set_hostname () {
  
  setup_elastic_sources() {
  	# Create Beats source list
-	echo "deb https://packages.elastic.co/beats/apt stable main" > /etc/apt/sources.list.d/beats.list
+	#echo "deb https://packages.elastic.co/beats/apt stable main" > /etc/apt/sources.list.d/beats.list
+	echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 
 	#Get Elasticsearch GPG key
 	wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | apt-key add -
